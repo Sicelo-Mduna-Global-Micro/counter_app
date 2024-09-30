39b1a223285cb09d67641553d266d6968f35445b
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-09-30 16:05:52 +0200

    Rendering Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,11 +4,26 @@ class Counter extends Component {
         count : 0,
 
      };
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        if(this.state.count === 0){
+            classes += 'warning';
+        }
+
+        else{
+            classes += 'primary';
+        }
+
+        return classes;
+
+    }
+
      
     render() { 
         return (
             <>
-                <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
             </>
         );
