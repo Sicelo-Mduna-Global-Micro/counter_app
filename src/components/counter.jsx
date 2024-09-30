4ea78fb127cfdd06e8c577044b2b5b4ef8b75022
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-09-30 13:44:37 +0200

    Embedding Expressions
    1) How to use the state object in order to create state variables
    
    2) how to create a custom function that manipulates the data in the state object that we created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 class Counter extends Component {
+    state = { 
+        count : 0
+     }; 
     render() { 
         return (
             <>
-                <h1>Hello World !</h1>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
             </>
         );
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        if(count === 0){
+            return("Zero")
+        }
+        else if(count > 0){
+            return(count)
+        }
+
+        else{
+            return("Negative Number")
+        }
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter'; 
+// import Counter from './components/counter'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter />
+    <App />
+    {/* <Counter /> */}
   </React.StrictMode>
 );
 
