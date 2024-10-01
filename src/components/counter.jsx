60be9ca02f366fc4f8c43c156b5ad823b9c87d94
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-01 14:36:25 +0200

    Composing Components

---
+++ src/components/counter.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+class Counter extends Component {
+    state = { 
+        count : 0
+
+     };
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        if(this.state.count === 0){
+            classes += 'warning';
+        }
+
+        else{
+            classes += 'primary';
+        }
+
+        return classes;
+
+    }
+
+    handleIncrement = () =>{
+        this.setState({count : this.state.count + 1});
+
+    }
+
+
+
+     
+    render() { 
+        return (
+            <>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick = {this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button><br></br>
+            </>
+        );
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        if(count === 0){
+            return("Zero")
+        }
+        else if(count > 0){
+            return(count)
+        }
+
+        else{
+            return("Negative Number")
+        }
+    }
+}
+ 
+export default Counter;
