d245b60326fda0f3c554a831a97e27170609afb0
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-02 23:06:00 +0200

    Destructing Arugments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import Counter from './counter';
 class Counters extends Component {
     
-    render() { 
+    render() {
+        const {onReset, onDelete, onIncrement} = this.props; 
         return (
             <>
                 <button
-                onClick = {this.props.onReset} 
+                onClick = {onReset} 
                 className='btn btn-primary btn-sm'>Reset
                 </button>
                 <div>
-                    {this.props.counters.map(counter =><Counter key = {counter.id} onDelete = {this.props.onDelete}  onIncrement = {this.props.onIncrement} counter = {counter}/>)} 
+                    {this.props.counters.map(counter =><Counter key = {counter.id} onDelete = {onDelete}  onIncrement = {onIncrement} counter = {counter}/>)} 
                 </div>
 
                 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,10 @@
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
     return ( 
         <>
                 <nav className ="navbar navbar-light bg-light">
                 <a className ="navbar-brand" href="#">Navbar
-                    <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+                    <span className="badge badge-pill badge-secondary">{totalCounters}</span>
                 </a>
                 </nav>
             </>
