48a4efd17a0048540e2d4d9c6200bda6ee031bad
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-02 14:18:44 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React, { Component } from 'react'
 import NavBar from './components/navbar';
 import Counters from './components/counters'; 
 import './App.css'; 
-function App() {
-  return (
-    <>
+class App extends Component {
+  render() { 
+    return (
+      <>
       <NavBar/>
       <main className='container'> 
         <Counters/>
       </main>
-    </>
-  );
-}
 
+      </>
+    );
+  }
+}
+ 
 export default App;
