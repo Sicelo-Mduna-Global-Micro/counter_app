4ca7914310f7bb38b51cc8f62aa425515436b2bc
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-02 22:53:42 +0200

    Stateless functional components

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -20,3 +20,4 @@ class Counters extends Component {
 }
  
 export default Counters;
+
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-class NavBar extends Component {
-    render() { 
-        return (
-            <>
+
+const NavBar = (props) => {
+    return ( 
+        <>
                 <nav className ="navbar navbar-light bg-light">
                 <a className ="navbar-brand" href="#">Navbar
-                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                    <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
                 </a>
                 </nav>
             </>
-        );
-    }
+     );
 }
  
-export default NavBar; 
+export default NavBar;
